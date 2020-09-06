3ddf5a3640fd744c7f1bf51a6e1cf450462e2b8e
JoniMujica <jonimujica3@hotmail.com>
2020-09-05 21:36:27 -0300

    cree las funciones push y pop para meter y sacar elementos de la pila

--- PILAS_AYED/pilas.cpp
+++ PILAS_AYED/pilas.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+struct Nodo
+{
+	int dato;
+	Nodo* siguiente;
+};
+
 int main() {
+	Nodo* pila = NULL;
 
 	return 0;
+}
+
+void push(Nodo*& pila, int dat) {
+	Nodo* Nuevo_nodo = new Nodo(); //pido un espacio en memoria para la nueva pila (nuevo nodo)
+	Nuevo_nodo->dato = dat; // cargo el valor dentro del nodo (campo 'dato')
+	Nuevo_nodo->siguiente = pila; // //carga el puntero pila dentro del nodo (*siguiente) [ya que pila era null]
+	pila = Nuevo_nodo; // asigna el nuevo nodo a pila
+}
+
+int pop(Nodo*& pila) { ////recibe la pila como par?metro y retorna el valor contenido en el primer nodo
+	int aux; //el tipo de dato de la informaci?n para retornarlo
+	Nodo* p = pila; //un puntero al comienzo de la estructura para luego eliminar ese nodo
+	aux = pila->dato; //	conservar el valor del primer nodo para retornarlo
+	pila = p->siguiente; //Avanzar con la pila un nodo para eliminar el nodo que estaba en el tope
+	delete p;	 //eliminar el que era primero, la pila esta apuntando al que era segundo o a NULL
+	return aux;
 }
